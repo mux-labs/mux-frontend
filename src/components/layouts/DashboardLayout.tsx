7d344c2314cc3b8945ce92b20d2dcbffc993fc25
tali-creator <talinanzing111@gmail.com>
2026-01-24 16:57:36 +0100

    chore: suppress lint warning for pathname dependency in DashboardLayout

--- src/components/layouts/DashboardLayout.tsx
+++ src/components/layouts/DashboardLayout.tsx
@@ -1,64 +1,62 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import { Sidebar } from './Sidebar'
-import { TopNav } from './TopNav'
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Sidebar } from "./Sidebar";
+import { TopNav } from "./TopNav";
 
 interface DashboardLayoutProps {
-  children: React.ReactNode
+	children: React.ReactNode;
 }
 
 export function DashboardLayout({ children }: DashboardLayoutProps) {
-  const [sidebarOpen, setSidebarOpen] = useState(false)
-  const pathname = usePathname()
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const pathname = usePathname();
 
-  useEffect(() => {
-    setSidebarOpen(false)
-  }, [pathname])
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		setSidebarOpen(false);
+	}, [pathname]);
 
-  useEffect(() => {
-    if (sidebarOpen) {
-      document.body.style.overflow = 'hidden'
-    } else {
-      document.body.style.overflow = 'auto'
-    }
-    
-    return () => {
-      document.body.style.overflow = 'auto'
-    }
-  }, [sidebarOpen])
+	useEffect(() => {
+		if (sidebarOpen) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "auto";
+		}
 
-  return (
-    <div className="relative flex min-h-screen bg-gray-50">
-      {/* Mobile Overlay */}
-      {sidebarOpen && (
-        <div 
-          className="fixed inset-0 z-40 bg-black/50 lg:hidden"
-          onClick={() => setSidebarOpen(false)}
-          aria-hidden="true"
-        />
-      )}
+		return () => {
+			document.body.style.overflow = "auto";
+		};
+	}, [sidebarOpen]);
 
-      {/* Sidebar */}
-      <Sidebar 
-        isOpen={sidebarOpen}
-        onClose={() => setSidebarOpen(false)}
-      />
-      
-      <div className="flex flex-col flex-1 ">
-        {/* TopNav */}
-        <TopNav onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
-        
-        {/* Main */}
-        <main className="flex-1">
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              {children}
-            </div>
-          </div>
-        </main>
-      </div>
-    </div>
-  )
+	return (
+		<div className="relative flex min-h-screen bg-gray-50">
+			{/* Mobile Overlay */}
+			{sidebarOpen && (
+				<div
+					className="fixed inset-0 z-40 bg-black/50 lg:hidden"
+					onClick={() => setSidebarOpen(false)}
+					aria-hidden="true"
+				/>
+			)}
+
+			{/* Sidebar */}
+			<Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+
+			<div className="flex flex-col flex-1 ">
+				{/* TopNav */}
+				<TopNav onMenuClick={() => setSidebarOpen(!sidebarOpen)} />
+
+				{/* Main */}
+				<main className="flex-1">
+					<div className="py-6">
+						<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+							{children}
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	);
 }
